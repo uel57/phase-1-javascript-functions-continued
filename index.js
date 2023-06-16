79a96653833329dfba050419779f92082545d9c9
samk <samuelkariuki1@student.moringaschool.com>
2023-06-16 14:57:17 +0300

    completed fuction tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+
+}
+function mondayWork(string="go to the office"){
+    return `This Monday, I will ${string}.`
+}
+function wrapAdjective(asterisk ="*"){
+    return function(adjective = "hard"){
+        return `You are ${asterisk}${adjective}${asterisk}!`
+    }
+}
 // code your solution here
